0556183c85fe7a6b91d4af50f35d0697e763a59f
Tamir Eilon <tamireilon@gmail.com>
2023-07-02 14:57:23 +0300

    managed

--- send_email.php
+++ send_email.php
@@ -1,31 +1,19 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
 
-  // Set the recipient email address
-  $to = "tamireilon@yahoo.com";
-
-  // Set the email subject
-  $subject = "New message from contact form";
-
-  // Set the email headers
-  $headers = "From: $name <$email>" . "\r\n";
-  $headers .= "Reply-To: $email" . "\r\n";
-  $headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
-
-  // Construct the email body
-  $body = "<h2>New message from contact form</h2>";
-  $body .= "<p><strong>Name:</strong> $name</p>";
-  $body .= "<p><strong>Email:</strong> $email</p>";
-  $body .= "<p><strong>Message:</strong> $message</p>";
+  // Set up the email parameters
+  $to = 'tamireilon@yahoo.com'; // Replace with your email address
+  $subject = 'New Contact Form Submission';
+  $body = "Name: $name\nEmail: $email\nMessage: $message";
 
   // Send the email
-  if (mail($to, $subject, $body, $headers)) {
-    echo "Thank you for your message!";
+  if (mail($to, $subject, $body)) {
+    echo 'Email sent successfully';
   } else {
-    echo "Oops! Something went wrong.";
+    echo 'Error sending email';
   }
 }
 ?>
