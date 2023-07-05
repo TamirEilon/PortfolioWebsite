ac43f83a2c345c94bde15227ea245beae82916ec
Tamir Eilon <134195272+TamirEilon@users.noreply.github.com>
2023-07-05 21:05:53 +0300

    Update clear_vm.sh

--- clear_vm.sh
+++ clear_vm.sh
@@ -4,10 +4,10 @@ echo 'Clearing up the VM'
 
 docker rm --force \$(docker ps -aq)
 
-// Deletes all images on the EC2 instance
+# Deletes all images on the EC2 instance
 docker rmi --force \$(docker images -q) 
 
-// Kills all runing dockers on the EC2 instance, in order to use the 80 port again
+# Kills all runing dockers on the EC2 instance, in order to use the 80 port again
 docker kill \$(docker ps -q) || true 
 
 docker rm -f websitecontainer
