f1a42ae9acf7d9776ca08b683b27dce5de592ff9
Tamir Eilon <134195272+TamirEilon@users.noreply.github.com>
2023-07-05 20:45:39 +0300

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-# Update the system packages
-sudo apt update
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
 
-# Install required dependencies
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo chmod +x get-docker.sh
 
-# Add the Docker repository GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-# Add the Docker repository
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Update the system packages again
-sudo apt update
-
-# Install Docker Engine
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-
-# Add the current user to the 'docker' group
 sudo usermod -aG docker $USER
 
-# Start Docker on system boot
 sudo systemctl enable docker
+
+sudo systemctl start docker
+
+echo Done installing Docker
